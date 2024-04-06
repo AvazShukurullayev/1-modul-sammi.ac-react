825b8c980fb78db3513cfc7440b751af0e1f0ae4
Avaz <elonarm21@gmail.com>
2024-04-06 18:14:26 +0500

    dynamic className added

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,14 +2,9 @@ import {AppInfo} from "../app-info/AppInfo.jsx";
 import {MovieList} from "../movie-list/MovieList.jsx";
 import {MovieAddForm} from "../movie-add-form/MovieAddForm.jsx";
 import {Filters} from "../filters/Filters.jsx";
-import {v4 as uuidv4} from 'uuid';
+import {data} from "../../movies.js";
 
 export const App = () => {
-    const data = [
-        {name: "Omar", viewers: 911, id: uuidv4()},
-        {name: "Mr. and Mrs.Smith", viewers: 1400, id: uuidv4()},
-        {name: "Empire of Ottoman", viewers: 385, id: uuidv4()}
-    ]
     return (
         <div className="app font-monospace container">
             <AppInfo/>
--- src/components/movie-list-item/MovieListItem.jsx
+++ src/components/movie-list-item/MovieListItem.jsx
@@ -2,12 +2,12 @@ import {Button, ButtonGroup, Col, Form, ListGroup, Row} from "react-bootstrap";
 import {Cookie, StarFill, Trash} from "react-bootstrap-icons";
 import PropTypes from "prop-types";
 
-export const MovieListItem = ({name, viewers}) => {
+export const MovieListItem = ({name, viewers, favourite, liked}) => {
     return (
         <ListGroup.Item as="li" className="">
             <Row className="align-items-center justify-content-between">
                 <Col md={7} style={{cursor: "pointer"}}>
-                    <span>{name}</span>
+                    <span className={`fs-5 ${favourite && 'text-warning'}`}>{name}</span>
                 </Col>
                 <Col md={4}>
                     <Row className="justify-content-between">
@@ -19,7 +19,7 @@ export const MovieListItem = ({name, viewers}) => {
                                 <Button variant="outline-warning" size="sm"><Cookie/></Button>
                                 <Button variant="outline-danger" size="sm"><Trash/></Button>
                             </ButtonGroup>
-                            <StarFill className="ms-2" color="yellow"/>
+                            <StarFill className={`ms-2 ${liked ? 'd-inline' : 'd-none'}`} color="yellow"/>
                         </Col>
                     </Row>
                 </Col>
@@ -30,5 +30,7 @@ export const MovieListItem = ({name, viewers}) => {
 
 MovieListItem.propTypes = {
     name: PropTypes.string,
-    viewers: PropTypes.number
+    viewers: PropTypes.number,
+    favourite: PropTypes.bool,
+    liked: PropTypes.bool
 }
