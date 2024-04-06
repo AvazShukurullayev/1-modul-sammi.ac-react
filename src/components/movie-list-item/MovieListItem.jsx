fd660244d2eafc9f621155a89080a85c3e48148b
Avaz <elonarm21@gmail.com>
2024-04-06 12:50:40 +0500

    shablon fixed

--- src/components/movie-list-item/MovieListItem.jsx
+++ src/components/movie-list-item/MovieListItem.jsx
@@ -1,13 +1,25 @@
-import {ListGroup} from "react-bootstrap";
+import {Button, Col, Form, ListGroup, Row} from "react-bootstrap";
 
 export const MovieListItem = () => {
     return (
-        <ListGroup.Item as="li">
-            <span>Umar</span>
-            <input type="number" defaultValue={989} />
-            <div>
-                <button></button>
-            </div>
+        <ListGroup.Item as="li" className="">
+            <Row>
+                <Col md={8}>
+                    <span>Umar</span>
+                </Col>
+                <Col md={4}>
+                    <Row className="justify-content-between">
+                        <Col md={9}>
+                            <Form.Control type="number" defaultValue={989} className="w-100"/>
+                        </Col>
+                        <Col md={3}>
+                            <div>
+                                <Button size="sm"></Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </ListGroup.Item>
     )
 }
