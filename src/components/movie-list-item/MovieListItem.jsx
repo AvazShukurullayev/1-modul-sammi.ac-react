977b4842104f0d10ac6ae94b1c52190bd526a836
Avaz <elonarm21@gmail.com>
2024-04-06 17:40:13 +0500

    react bootstrap icon added

--- src/components/movie-list-item/MovieListItem.jsx
+++ src/components/movie-list-item/MovieListItem.jsx
@@ -1,21 +1,24 @@
-import {Button, Col, Form, ListGroup, Row} from "react-bootstrap";
+import {Button, ButtonGroup, Col, Form, ListGroup, Row} from "react-bootstrap";
+import {Cookie, StarFill, Trash} from "react-bootstrap-icons";
 
 export const MovieListItem = () => {
     return (
         <ListGroup.Item as="li" className="">
-            <Row>
-                <Col md={8}>
+            <Row className="align-items-center justify-content-between">
+                <Col md={7} style={{cursor: "pointer"}}>
                     <span>Umar</span>
                 </Col>
                 <Col md={4}>
                     <Row className="justify-content-between">
-                        <Col md={9}>
-                            <Form.Control type="number" defaultValue={989} className="w-100"/>
+                        <Col md={7}>
+                            <Form.Control type="number" defaultValue={989} className="text-center"/>
                         </Col>
-                        <Col md={3}>
-                            <div>
-                                <Button size="sm"></Button>
-                            </div>
+                        <Col md={5}>
+                            <ButtonGroup>
+                                <Button variant="outline-warning" size="sm"><Cookie/></Button>
+                                <Button variant="outline-danger" size="sm"><Trash/></Button>
+                            </ButtonGroup>
+                            <StarFill className="ms-2" color="yellow"/>
                         </Col>
                     </Row>
                 </Col>
